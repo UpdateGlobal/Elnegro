dc7e6d1cdccb419f04ed9f7d798ab6a0bd6d5463
luizink <luizupdate@gmail.com>
2018-03-26 09:23:19 -0500

    slider

--- layout/slide.php
+++ layout/slide.php
@@ -7,14 +7,14 @@
                     <h3 class="pogoSlider-slide-element" data-in="slideUp" data-out="flipX" data-duration="700"><a  href="#historia">Bienvenidos</a></h3>
                 </div>
                 <div class="pogoSlider-slide" data-transition="fade" style="background:url('picture/foto-home2.jpg') no-repeat scroll 0 0 / cover;">
-                    <h2 class="pogoSlider-slide-element" data-in="slideDown" data-out="flipX" data-duration="700">Our menus</h2>
-                    <h1 class="pogoSlider-slide-element" data-in="expand" data-out="flipX" data-duration="1500">See What's <span>new</span> today</h1>
-                    <h3 class="pogoSlider-slide-element" data-in="slideUp" data-out="flipX" data-duration="700"><a href="index.html#menu">Todays Menu</a></h3>
+                    <h2 class="pogoSlider-slide-element" data-in="slideDown" data-out="flipX" data-duration="700">El Negro</h2>
+                    <h1 class="pogoSlider-slide-element" data-in="expand" data-out="flipX" data-duration="1500"><span>frutos Secos</span> Peruanos</h1>
+                    <h3 class="pogoSlider-slide-element" data-in="slideUp" data-out="flipX" data-duration="700"><a  href="#historia">Bienvenidos</a></h3>
                 </div>
                 <div class="pogoSlider-slide" data-transition="fade" style="background:url('picture/foto-home3.jpg') no-repeat scroll 0 0 / cover;">
-                    <h2 class="pogoSlider-slide-element" data-in="slideDown" data-out="flipX" data-duration="700">Get ready</h2>
-                    <h1 class="pogoSlider-slide-element" data-in="expand" data-out="flipX" data-duration="1500">to <span>Join</span> with us</h1>
-                    <h3 class="pogoSlider-slide-element" data-in="slideUp" data-out="flipX" data-duration="700"><a  href="index.html#reservation-form-modal" data-toggle="modal">Book A Table</a></h3>
+                    <h2 class="pogoSlider-slide-element" data-in="slideDown" data-out="flipX" data-duration="700">El Negro</h2>
+                    <h1 class="pogoSlider-slide-element" data-in="expand" data-out="flipX" data-duration="1500"><span>frutos Secos</span> Peruanos</h1>
+                    <h3 class="pogoSlider-slide-element" data-in="slideUp" data-out="flipX" data-duration="700"><a  href="#historia">Bienvenidos</a></h3>
                 </div>
             </div>
         </div>
